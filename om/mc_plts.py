a9229581a8c957f7820462e46ef3d48f2f856dbd
Tom Donoghue <tdonoghue@ucsd.edu>
2016-09-29 14:01:58 +0530

    ENH: Added skeleton code for mc plotting

---
+++ om/mc_plts.py
@@ -0,0 +1,14 @@
+from __future__ import print_function, division
+import matplotlib.pyplot as plt
+
+from om.gen import FigInfo
+
+##################################################################
+################ OM - PLTS - MAP COMPARISON PLOTS ################
+##################################################################
+
+def plot_corrs():
+    pass
+
+def plot_con_mat():
+    pass
