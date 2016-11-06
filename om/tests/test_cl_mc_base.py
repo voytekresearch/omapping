d4a5785562c8585390b3d210f2086eba74764d84
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-05 21:23:59 -0700

    Update separated test file for MC base after MC reorganization

--- om/tests/test_cl_mc_base.py
+++ om/tests/test_cl_mc_base.py
@@ -10,12 +10,8 @@ def test_mc_base():
     """   """
 
     db = OMDB()
-    assert MapCompBase(db)
+    assert mc.MapCompBase(db)
 
-def test_get_map_names():
-    """   """
-
-    pass
 
 def test_init_meg_map_dict():
     """   """
@@ -25,22 +21,6 @@ def test_init_meg_map_dict():
     assert mc._init_meg_map_dict(bands)
     assert mc._init_meg_map_dict(bands, 100)
 
-def test_init_stat_dict():
-    """   """
 
-    assert mc._init_stat_dict(['a', 'b', 'c'])
 
-def test_mat_mult():
-    """   """
 
-    pass
-
-def test_make_list():
-    """   """
-
-    pass
-
-def test_pull_out_results():
-    """   """
-
-    pass
