9193c0e7bd245e5c22d2bb03b8e781b69f7d719d
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-22 00:24:26 -0800

    Add function to load meg group

--- om/tests/helper_test_funcs.py
+++ om/tests/helper_test_funcs.py
@@ -4,7 +4,8 @@ from __future__ import print_function, division
 
 import os
 
-import om.cl.md_sing as md
+import om.cl.md_sing as md_sing
+import om.cl.md_gr as md_gr
 from om.gen import Osc
 
 ##
@@ -33,8 +34,29 @@ def load_test_meg_subj(sub):
     db = TestDB()
     osc = Osc(default=True)
 
-    dat = md.MegData(db, osc)
+    dat = md_sing.MegData(db, osc)
 
     dat.import_foof(sub, get_demo=False, load_type='pickle')
 
     return dat
+
+def load_test_meg_gr(bands_vertex=False):
+    """Loads a test group object of MD_GR data."""
+
+    db = TestDB()
+    osc = Osc(default=True)
+
+    meg_group = md_gr.GroupMegData(db, osc)
+
+    subjs = ['test2', 'test2']
+
+    for s in subjs:
+
+        meg_subj = load_test_meg_subj(s)
+
+        if bands_vertex:
+            meg_subj.osc_bands_vertex()
+
+        meg_group.add_subject(meg_subj, add_vertex_bands=bands_vertex)
+
+    return meg_group
