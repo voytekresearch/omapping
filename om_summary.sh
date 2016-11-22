0fb46547a854b08414a5a3dd0c393b8c18d7a656
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-21 16:54:59 -0800

    Update cloc to ignore test data files, and check code and tests separately

--- om_summary.sh
+++ om_summary.sh
@@ -6,8 +6,11 @@ printf "\nNumber of lines of code & comments in OM: "
 find ./om -name "*.py" -type f -exec grep . {} \; | wc -l
 
 # Check number of files using cloc
-printf "\n\n\n CLOC OUTPUT: \n"
-cloc om
+printf "\n\n\n CLOC OUTPUT (EXCLUDING TESTS): \n"
+cloc om --exclude-dir='tests'
+
+printf "\n\n\n TEST FILES: \n"
+cloc om/tests --exclude-dir='test_files'
 
 # Run Tests & Check Coverage
 printf "\n\n\n RUN TESTS & CHECK COVERAGE: \n"
