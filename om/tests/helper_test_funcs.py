96a61aca9a03e669e595884bad6a15132cd5da58
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-21 16:40:22 -0800

    Add file of helper functions for testing

---
+++ om/tests/helper_test_funcs.py
@@ -0,0 +1,36 @@
+"""   """
+
+from __future__ import print_function, division
+
+import os
+
+import om.cl.md_sing as md
+
+##
+##
+##
+
+class TestDB(object):
+
+    def __init__(self):
+        """    """
+
+        self.dat_source = 'test'
+        self.project_path = ("/Users/thomasdonoghue/Documents/GitCode/omegamappin/om/tests/test_files/")
+
+        self.foof_path = os.path.join(self.project_path, 'foof')
+
+##
+##
+##
+
+def load_test_meg_subj(sub):
+    """Loads a test subject of MD_SING data."""
+
+    db = TestDB()
+
+    dat = md.MegData(db)
+
+    dat.import_foof(sub, get_demo=False, load_type='pickle')
+
+    return dat
