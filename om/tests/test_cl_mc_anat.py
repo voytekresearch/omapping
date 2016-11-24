91bb0d64b0dcba13f6a157577dd9549825a8737d
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-24 12:23:17 -0800

    Add more test coverage

--- om/tests/test_cl_mc_anat.py
+++ om/tests/test_cl_mc_anat.py
@@ -5,6 +5,7 @@ from py.test import raises
 
 from om.gen import OMDB
 import om.cl.mc_anat as mc
+from helper_test_funcs import TestDB as TDB
 
 ##############################################################################
 ################# TESTS - OMEGAMAPPIN - CL_MC_ANAT - CLASSES #################
@@ -63,7 +64,7 @@ def test_roi_set_comment():
     assert roi.comment
 
 def test_check_consistency1():
-    """   """
+    """Test check consistency, with label and lr data."""
 
     roi = mc.ROI()
 
@@ -80,6 +81,7 @@ def test_check_consistency1():
         assert roi.check_consistency()
 
 def test_check_consistency2():
+    """Test check consistency, with label, lr and verts data."""
 
     roi = mc.ROI()
 
@@ -106,11 +108,11 @@ def test_mc_anat():
 
     assert mc.MapCompAnat(db)
 
-##################################################################################
-################### TESTS - OMEGAMAPPIN - CL_MC_ANAT - CLASSES ###################
-##################################################################################
+####################################################################################
+################### TESTS - OMEGAMAPPIN - CL_MC_ANAT - FUNCTIONS ###################
+####################################################################################
 
-def test_extrat_lr_anat():
+def test_extract_lr_anat():
     """   """
 
     anat_labels = ['left_caudal_anterior_cingulate',
@@ -124,7 +126,6 @@ def test_extrat_lr_anat():
     assert labels[1] == 'caudalmiddlefrontal'
     assert lrs[0] is 'L'
     assert lrs[1] is 'R'
-
 
 def test_extract_lr_elec():
     """   """
@@ -140,7 +141,7 @@ def test_extract_lr_elec():
     assert lrs[0] is 'R'
     assert lrs[1] is 'L'
 
-def test_clean_label_elec():
+def test_clean_label_anat():
     """   """
 
     label_1 = 'left_caudal_anterior_cingulate'
@@ -175,22 +176,93 @@ def test_mat_mult():
     assert res.shape == (3, 3)
     assert np.all(res == exp)
 
-
 ####################################################################################
 ################# TESTS - OMEGAMAPPIN - CL_MC_ANAT - ROI FUNCTIONS #################
 ####################################################################################
 
 def test_load_anat_maps():
-    pass
 
-def test_load_elec_maps():
-    pass
+    tdb = TDB()
+
+    map_comp = mc.MapCompAnat(tdb)
+
+    map_comp.load_anat_maps('test_anat.mat', 'test')
+
+    assert map_comp.anat.loaded
+
+def test_load_elec_rois():
+
+    tdb = TDB()
+
+    map_comp = mc.MapCompAnat(tdb)
+
+    map_comp.load_elec_rois('test_scout.mat')
+
+    assert map_comp.elec.loaded
+
+def test_load_elec_rois_default():
+
+    db = OMDB()
+
+    map_comp = mc.MapCompAnat(db)
+    map_comp.load_elec_rois()
+
+    assert map_comp.elec.loaded
 
 def test_align_rois():
-    pass
+
+    tdb = TDB()
+
+    map_comp = mc.MapCompAnat(tdb)
+
+    map_comp.load_anat_maps('test_anat.mat', 'test')
+    map_comp.load_elec_rois('test_scout.mat')
+
+    map_comp.align_rois()
+
+    assert map_comp.rois.loaded
+
+def test_align_rois_errors():
+
+    tdb = TDB()
+
+    map_comp = mc.MapCompAnat(tdb)
+
+    with raises(mc.DataNotComputedError):
+        assert map_comp.align_rois()
 
 def test_conv_meg_rois():
-    pass
+
+    tdb = TDB()
+
+    map_comp = mc.MapCompAnat(tdb)
+
+    map_comp.load_meg_maps('test_meg')
+
+    map_comp.load_anat_maps('test_anat.mat', 'test')
+    map_comp.load_elec_rois('test_scout.mat')
+
+    map_comp.align_rois()
+
+    map_comp.conv_meg_rois()
+
+    assert True
 
 def test_comp_meg_anat():
-    pass
+
+    tdb = TDB()
+
+    map_comp = mc.MapCompAnat(tdb)
+
+    map_comp.load_meg_maps('test_meg')
+
+    map_comp.load_anat_maps('test_anat.mat', 'test')
+    map_comp.load_elec_rois('test_scout.mat')
+
+    map_comp.align_rois()
+
+    map_comp.conv_meg_rois()
+
+    map_comp.comp_meg_anat()
+
+    assert True
