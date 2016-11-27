9ecfd29eb40c4eb140f030c3ae2d0ca74d373079
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-26 23:18:53 -0800

    ReOrg: add file in core for custom errors

---
+++ om/core/errors.py
@@ -0,0 +1,21 @@
+"""   """
+
+#########################################################################################
+###################################### OM GEN - ERRORS ##################################
+#########################################################################################
+
+class UnknownDataSourceError(Exception):
+    """An Error indicating data source specification is not understood."""
+    pass
+
+class UnknownDataTypeError(Exception):
+    """An Error indicating data type specification is not understood."""
+    pass
+
+class InconsistentDataError(Exception):
+    """An Error indicating there is a fatal inconsistency in data."""
+    pass
+
+class DataNotComputedError(Exception):
+    """An Error indicating some required data has not been computed."""
+    pass
