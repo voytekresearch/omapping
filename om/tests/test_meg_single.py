bf9203745bf852f1a069c1716299559434d1bad8
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-27 00:17:09 -0800

    ReOrg: separate out testing private functions

--- om/tests/test_meg_single.py
+++ om/tests/test_meg_single.py
@@ -1,6 +1,5 @@
-from __future__ import print_function, division
+"""   """
 
-import os
 import numpy as np
 from py.test import raises
 
@@ -10,27 +9,16 @@ from om.tests.utils import TestDB as TDB
 from om.tests.utils import load_test_meg_subj
 
 #import om.cl.md_sing as md
-import om.meg.single as md
+#import om.meg.single as md
+from om.meg.single import *
 
 #from om.gen import OMDB, Osc
 from om.core.db import OMDB
 from om.core.osc import Osc
 
-
-######################################################################################
-##################### TESTS - OMEGAMAPPIN - CL_MD_SING - CLASSES #####################
-######################################################################################
-
-def test_meg_data():
-    """   """
-
-    db = OMDB()
-
-    assert md.MegData(db, '')
-
-###########################################################################################
-################### TESTS - OMEGAMAPPIN - CL_MD_SING - PUBLIC FUNCTIONS ###################
-###########################################################################################
+########################################################################################
+#################### TESTS - OMEGAMAPPIN - MEG - SINGLE - FUNCTIONS ####################
+########################################################################################
 
 def test_print_corrs_mat():
 
@@ -38,7 +26,7 @@ def test_print_corrs_mat():
     ps_dat = np.array([[0.1, 0.1], [0.1, 0.1]])
     labels = ['a', 'b']
 
-    md.print_corrs_mat(rs_dat, ps_dat, labels)
+    print_corrs_mat(rs_dat, ps_dat, labels)
 
     assert True
 
@@ -50,114 +38,26 @@ def test_print_corrs_vec():
     labels = ['a', 'b']
     desc = 'dat'
 
-    md.print_corrs_vec(rs_dat, ps_dat, labels, desc)
+    print_corrs_vec(rs_dat, ps_dat, labels, desc)
 
     assert True
 
-##########################################################################################
-################## TESTS - OMEGAMAPPIN - CL_MD_SING - PRIVATE FUNCTIONS ##################
-##########################################################################################
+######################################################################################
+######################### TESTS - OMEGAMAPPIN - MEG - SINGLE #########################
+######################################################################################
 
-def test_get_single_osc():
-    """   """
-
-    cens = np.array([3, 6, 9, 12, 15])
-    pows = np.array([1, 1, 2, 1, 1])
-    bws = np.array([1, 1, 1, 1, 1])
-
-    osc_l = 5
-    osc_h = 10
-
-    out_dat = md._get_single_osc(cens, pows, bws, osc_l, osc_h)
-
-    assert np.array_equal(out_dat, np.array([9, 2, 1, 2]))
-
-def test_get_single_osc_power_none():
-    """   """
-
-    cens = np.array([])
-    pows = np.array([])
-    bws = np.array([])
-
-    out_cen, out_pow, out_bw = md._get_single_osc_power(cens, pows, bws)
-
-    assert out_cen == 0.
-    assert out_pow == 0.
-    assert out_bw == 0.
-
-def test_get_single_osc_power_one():
-
-    cens = np.array([10.])
-    pows = np.array([1.])
-    bws = np.array([1.])
-
-    out_cen, out_pow, out_bw = md._get_single_osc_power(cens, pows, bws)
-
-    assert out_cen == 10.
-    assert out_pow == 1.
-    assert out_bw == 1.
-
-def test_get_single_osc_power_mult():
-
-    cens = np.array([10., 11.])
-    pows = np.array([1., 2.])
-    bws = np.array([1., 1.])
-
-    out_cen, out_pow, out_bw = md._get_single_osc_power(cens, pows, bws)
-
-    assert out_cen == 11.
-    assert out_pow == 2.
-    assert out_bw == 1.
-
-def test_get_demo_csv_omega():
+def test_meg_data():
     """   """
 
     db = OMDB()
 
-    subnum = 220216
-    dat_source = 'OMEGA'
-
-    sex, age = md._get_demo_csv(subnum, db.meg_path, dat_source)
-
-    assert sex == 'M'
-    assert age == 30
-
-def test_get_demo_csv_hcp():
-    """   """
-
-    db = OMDB()
-
-    subnum = 146129
-    dat_source = 'HCP'
-
-    sex, age = md._get_demo_csv(subnum, db.meg_path, dat_source)
-
-    assert sex == 'M'
-    assert age == 23.5
-
-def test_osc_peak():
-    """   """
-
-    centers = np.array([6, 9, 10, 12, 15])
-    osc_low = 8
-    osc_high = 13
-
-    test_ans = md._osc_peak(centers, osc_low, osc_high)
-
-    assert np.isclose(test_ans, 10.33, rtol=0.01)
-
-    test_ans = md._osc_peak(centers, osc_low, osc_high, 'median')
-    assert np.isclose(test_ans, 10.00, rtol=0.01)
-
-##########################################################################################
-################### TESTS - OMEGAMAPPIN - CL_MD_SING - CLASS FUNCTIONS ###################
-##########################################################################################
+    assert MegData(db, '')
 
 def test_cl_set_bands():
 
     tdb = TDB()
 
-    meg_dat = md.MegData(tdb, '')
+    meg_dat = MegData(tdb, '')
 
     osc = Osc(default=True)
 
@@ -169,18 +69,18 @@ def test_cl_set_bands_error():
 
     osc = Osc(default=True)
 
-    meg_dat = md.MegData(tdb, '', osc=osc)
+    meg_dat = MegData(tdb, '', osc=osc)
 
     meg_dat.bands_vertex = True
 
-    with raises(md.InconsistentDataError):
+    with raises(InconsistentDataError):
         meg_dat.set_bands(osc)
 
 def test_cl_import_foof():
 
     tdb = TDB()
 
-    meg_dat = md.MegData(tdb, '')
+    meg_dat = MegData(tdb, '')
 
     meg_dat.import_foof('test_v2', get_demo=False, load_type='pickle')
 
@@ -196,10 +96,10 @@ def test_cl_import_foof_error():
 
     tdb = TDB()
 
-    meg_dat = md.MegData(tdb, '')
+    meg_dat = MegData(tdb, '')
     meg_dat.import_foof('test_v2', get_demo=False, load_type='pickle')
 
-    with raises(md.InconsistentDataError):
+    with raises(InconsistentDataError):
         meg_dat.import_foof('test_v5', get_demo=False, load_type='pickle')
 
 def test_cl_osc_bands_vertex():
@@ -215,9 +115,9 @@ def tests_cl_osc_bands_vertex_error():
 
     tdb = TDB()
 
-    meg_dat = md.MegData(tdb, '')
+    meg_dat = MegData(tdb, '')
 
-    with raises(md.DataNotComputedError):
+    with raises(DataNotComputedError):
         meg_dat.osc_bands_vertex()
 
 def test_cl_all_oscs():
@@ -255,14 +155,14 @@ def test_cl_peak_freq_errors():
 
     tdb = TDB()
 
-    meg_dat = md.MegData(tdb, '')
+    meg_dat = MegData(tdb, '')
 
-    with raises(md.DataNotComputedError):
+    with raises(DataNotComputedError):
         meg_dat.peak_freq()
 
     meg_dat.all_osc = True
 
-    with raises(md.DataNotComputedError):
+    with raises(DataNotComputedError):
         meg_dat.peak_freq()
 
 def test_cl_calc_osc_param_corrs():
@@ -280,7 +180,7 @@ def test_cl_calc_osc_param_corrs_error():
 
     meg_dat = load_test_meg_subj('test_v2')
 
-    with raises(md.DataNotComputedError):
+    with raises(DataNotComputedError):
         meg_dat.calc_osc_param_corrs()
 
 def test_cl_set_foof_viz():
