811e1303e1f1d1d6341b93b675333b5a32d8c6f7
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-27 00:18:33 -0800

    ReOrg: move out roi stuff

--- om/maps/anat.py
+++ om/maps/anat.py
@@ -16,81 +16,11 @@ from om.core.utils import get_section
 
 #from om.cl.mc_base import MapCompBase, _init_meg_map_dict
 from om.maps.base import MapCompBase, _init_meg_map_dict
+from om.maps.roi import ROI
 
 ####################################################################################
 #################### OMEGAMAPPIN - MAP COMPARE - ANAT - CLASSES ####################
 ####################################################################################
-
-class ROI(object):
-    """Class to hold data about ROIs.
-
-    Attributes
-    ----------
-    n_rois : int
-        The number of ROIs.
-    labels : list of str
-        Label for each ROI.
-    verts : list of array
-        List of vertices included in the ROI.
-    lr : list of str
-        L/R for each ROI.
-    comment : str
-        Comment about the ROIs.
-
-    Notes
-    -----
-    -
-    """
-
-    def __init__(self):
-        """   """
-
-        # Initialize fields to store ROI data
-        self.n_rois = int()
-        self.labels = list()
-        self.lrs = list()
-        self.verts = list()
-        self.comment = str()
-        self.loaded = bool()
-
-
-    def set_labels(self, labels, lrs):
-        """   """
-
-        # Check labels and lrs are the same size
-        if len(labels) != len(lrs):
-            raise InconsistentDataError('Input labels and lrs do not match!')
-        self.labels = labels
-        self.lrs = lrs
-        self.n_rois = len(labels)
-        self.loaded = True
-
-
-    def set_verts(self, verts):
-        """   """
-
-        verts_fixed = []
-
-        for vert in verts:
-            self.verts.append(np.squeeze(vert - 1))
-
-
-    def set_comment(self, comment):
-        """   """
-
-        self.comment = comment
-
-
-    def check_consistency(self):
-        """   """
-
-        if not (self.n_rois == len(self.labels) == len(self.lrs)):
-            raise InconsistentDataError('Discrepancy in number of labels and/or L/Rs.')
-
-        if self.verts:
-            if not (self.n_rois == len(self.verts)):
-                raise InconsistentDataError('Discrepancy in the number of vertices.')
-
 
 class MapCompAnat(MapCompBase):
     """Class for storing and comparing spatial topographies in ROIs.
