34ce20ada0a419232123f56c83b10af62161be2c
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-27 00:05:51 -0800

    ReOrg: fix up imports

--- om/tests/test_maps_tg_priv.py
+++ om/tests/test_maps_tg_priv.py
@@ -1,4 +1,14 @@
+"""   """
 
+import os
+import csv
+import numpy as np
+import pandas as pd
+from types import StringType, ListType
+
+import om.maps.tg as tg
+from om.core.db import OMDB
+from om.tests.utils import TestDB as TDB
 
 ####################################################################################
 ################ TESTS - OMEGAMAPPIN - CL_MC_TG - PRIVATE FUNCTIONS ################
@@ -10,7 +20,7 @@ def test_get_map_names_terms():
     db = OMDB()
 
     names_file = '00-ns_terms.csv'
-    names = mc._get_map_names(names_file, os.path.join(db.maps_path, 'Terms'))
+    names = tg._get_map_names(names_file, os.path.join(db.maps_path, 'Terms'))
 
     assert names
     assert type(names) == ListType
@@ -22,7 +32,7 @@ def test_get_map_names_genes():
     db = OMDB()
 
     names_file = '00-real_gene_names.csv'
-    names = mc._get_map_names(names_file, os.path.join(db.maps_path, 'Genes'))
+    names = tg._get_map_names(names_file, os.path.join(db.maps_path, 'Genes'))
 
     assert names
     assert type(names) is ListType
@@ -33,7 +43,7 @@ def test_get_gene_files():
 
     db = OMDB()
 
-    genes_files_path = mc._get_gene_files('sub1', db)
+    genes_files_path = tg._get_gene_files('sub1', db)
 
     assert genes_files_path
     assert type(genes_files_path) is ListType
@@ -50,7 +60,7 @@ def test_avg_csv_files():
 
     f_out = os.path.join(tdb.csvs_path, 'test_out.csv')
 
-    mc._avg_csv_files(f_in, f_out)
+    tg._avg_csv_files(f_in, f_out)
 
     assert os.path.exists(f_out)
 
@@ -66,7 +76,7 @@ def test_avg_csv_files():
     f.close()
     os.remove(f_out)
 
-    mc._avg_csv_files(f_in, f_out, 'median')
+    tg._avg_csv_files(f_in, f_out, 'median')
     assert os.path.exists(f_out)
     os.remove(f_out)
 
@@ -74,7 +84,7 @@ def test_init_stat_dict():
     """   """
 
     bands = ['a', 'b', 'c']
-    d = mc._init_stat_dict(bands)
+    d = tg._init_stat_dict(bands)
 
     assert d
     bands.append('Slopes')
@@ -85,7 +95,7 @@ def test_make_list():
 
     df = pd.DataFrame(np.array([[1, 2], [1, 2]]))
 
-    l = mc._make_list(df)
+    l = tg._make_list(df)
 
     assert len(l) == 2
     assert np.array_equal(l[0], np.array([1, 1]))
@@ -96,7 +106,7 @@ def test_pull_out_results():
 
     dat = [(1, 2), (1, 2)]
 
-    out_1, out_2 = mc._pull_out_results(dat)
+    out_1, out_2 = tg._pull_out_results(dat)
 
     assert np.array_equal(out_1, np.array([1, 1]))
     assert np.array_equal(out_2, np.array([2, 2]))
