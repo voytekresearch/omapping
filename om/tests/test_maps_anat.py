6ca1ae13fb8d1825693d0686c755c99a2de0ca7f
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-06 12:31:56 -0800

    Add in private tests. Also, fix for separation of calc and comp, and update all to use load_anat util

--- om/tests/test_maps_anat.py
+++ om/tests/test_maps_anat.py
@@ -6,6 +6,9 @@ from py.test import raises
 from om.maps.anat import *
 from om.core.db import OMDB
 from om.tests.utils import TestDB as TDB
+from om.tests.utils import load_test_anat
+
+from om.maps.anat import _extract_lr, _clean_label, _mat_mult
 
 #####################################################################################
 ######################### TESTS - OMEGAMAPPIN - MAPS - ANAT #########################
@@ -49,12 +52,7 @@ def test_load_elec_rois_default():
 
 def test_align_rois():
 
-    tdb = TDB()
-
-    map_comp = MapCompAnat(tdb)
-
-    map_comp.load_anat_maps('test_anat.mat', 'test')
-    map_comp.load_elec_rois('test_scout.mat')
+    map_comp = load_test_anat(load_anat=True, load_scout=True)
 
     map_comp.align_rois()
 
@@ -71,36 +69,99 @@ def test_align_rois_errors():
 
 def test_conv_meg_rois():
 
-    tdb = TDB()
 
-    map_comp = MapCompAnat(tdb)
-
-    map_comp.load_meg_maps('test_meg')
-
-    map_comp.load_anat_maps('test_anat.mat', 'test')
-    map_comp.load_elec_rois('test_scout.mat')
-
-    map_comp.align_rois()
+    map_comp = load_test_anat(load_meg=True, load_scout=True, load_anat=True,
+                              align=True)
 
     map_comp.conv_meg_rois()
+
+    assert True
+
+def test_calc_meg_con():
+    """   """
+
+    map_comp = load_test_anat(load_meg=True, load_scout=True, load_anat=True,
+                              align=True, convert=True)
+
+    map_comp.calc_meg_con()
 
     assert True
 
 def test_comp_meg_anat():
 
-    tdb = TDB()
-
-    map_comp = MapCompAnat(tdb)
-
-    map_comp.load_meg_maps('test_meg')
-
-    map_comp.load_anat_maps('test_anat.mat', 'test')
-    map_comp.load_elec_rois('test_scout.mat')
-
-    map_comp.align_rois()
-
-    map_comp.conv_meg_rois()
+    map_comp = load_test_anat(load_meg=True, load_scout=True, load_anat=True,
+                              align=True, convert=True, calc_meg=True)
 
     map_comp.comp_meg_anat()
 
     assert True
+
+######################################################################################
+################### TESTS - OMEGAMAPPIN - ANAT - PRIVATE FUNCTIONS ###################
+######################################################################################
+
+
+def test_extract_lr_anat():
+    """   """
+
+    anat_labels = ['left_caudal_anterior_cingulate',
+                   'right_caudal_middle_frontal']
+
+    labels, lrs = _extract_lr(anat_labels, 'anat')
+
+    assert len(labels) == 2
+    assert len(lrs) == 2
+    assert labels[0] == 'caudalanteriorcingulate'
+    assert labels[1] == 'caudalmiddlefrontal'
+    assert lrs[0] is 'L'
+    assert lrs[1] is 'R'
+
+def test_extract_lr_elec():
+    """   """
+
+    elec_labels = ['parsopercularis R', 'postcentral L',]
+
+    labels, lrs = _extract_lr(elec_labels, 'elec')
+
+    assert len(labels) == 2
+    assert len(lrs) == 2
+    assert labels[0] == 'parsopercularis'
+    assert labels[1] == 'postcentral'
+    assert lrs[0] is 'R'
+    assert lrs[1] is 'L'
+
+def test_clean_label_anat():
+    """   """
+
+    label_1 = 'left_caudal_anterior_cingulate'
+    label_2 = 'right_caudal_anterior_cingulate'
+    lr_1 = 'L'
+    lr_2 = 'R'
+
+    label_1 = _clean_label(label_1, lr_1, 'anat')
+    label_2 = _clean_label(label_2, lr_2, 'anat')
+
+    assert label_1 == 'caudalanteriorcingulate'
+    assert label_2 == 'caudalanteriorcingulate'
+
+def test_clean_label_elec():
+    """   """
+
+    label = 'parsopercularis R'
+    lr = 'R'
+
+    label = _clean_label(label, lr, 'elec')
+
+    assert label == 'parsopercularis'
+
+def test_mat_mult():
+    """   """
+
+    dat = np.array([1, 2, 3])
+
+    res = _mat_mult(dat)
+    exp = np.array([[1, 2, 3], [2, 4, 6], [3, 6, 9]])
+
+    assert res.shape == (3, 3)
+    assert np.all(res == exp)
+
