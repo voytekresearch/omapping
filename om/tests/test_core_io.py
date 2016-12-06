51dedba03f7a10576cd7ca95ab4bcf3ebee519d9
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-06 14:03:11 -0800

    add more testing of io funcs

--- om/tests/test_core_io.py
+++ om/tests/test_core_io.py
@@ -1,22 +1,20 @@
 """   """
 
 import numpy as np
+from py.test import raises
 
 from om.core.io import *
 from om.tests.utils import TestDB as TDB
 from om.meg.single import MegData
 
-##
-##
-##
+##########################################################################################
+##########################################################################################
+##########################################################################################
 
 def test_load_meg_psds():
     """   """
 
     tdb = TDB()
-    pass
-
-def test_load_foof_pickle():
     pass
 
 def test_save_foof_pickle():
@@ -31,7 +29,17 @@ def test_save_foof_pickle():
 
     assert os.path.exists(os.path.join(tdb.foof_path, 'pickle', '999_Foof_Vertex.p'))
 
+def test_load_foof_pickle():
+    """   """
+
+    tdb = TDB()
+
+    results = load_foof_pickle(tdb.foof_path, 999)
+
+    assert results
+
 def test_save_foof_csv():
+    """   """
 
     tdb = TDB()
 
@@ -53,8 +61,14 @@ def test_save_obj_pickle():
 
     assert True
 
+    with raises(UnknownDataTypeError):
+        save_obj_pickle(dat, 'BAD', 'test', db=tdb)
+
 def test_load_obj_pickle():
 
     tdb = TDB()
 
     assert load_obj_pickle('meg', 'test', db=tdb)
+
+    with raises(UnknownDataTypeError):
+        load_obj_pickle('BAD', 'test', db=tdb)
