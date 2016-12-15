b2d36de8c429cc42e9f67aec2e214c2c05a1b6b1
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-14 18:10:29 -0800

    Initialize file and add initial tests for decoding

---
+++ om/tests/test_meg_decoding.py
@@ -0,0 +1,36 @@
+"""DOCSTRING"""
+
+from py.test import raises
+
+from om.meg.decoding import *
+
+##
+##
+##
+
+def test_load_subjs():
+    pass
+
+def test_split_inds():
+    """   """
+
+    train_inds, test_inds = split_inds(100, 80, 20)
+
+    assert len(set(train_inds)) == 80
+    assert len(set(test_inds)) == 20
+
+    # Check there is no overlap in indices
+    assert set(train_inds).isdisjoint(set(test_inds))
+
+    # Check error catching
+    with raises(InconsistentDataError):
+        assert split_inds(80, 80, 20)
+
+def test_check_accuracy_single():
+    pass
+
+def test_check_accuracy_all():
+    pass
+
+def test_knn():
+    pass
