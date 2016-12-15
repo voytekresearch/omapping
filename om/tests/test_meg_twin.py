162067a3330cfb575c2b4934316393b8981273b5
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-14 18:10:08 -0800

    Initialize file for testing meg twin functions

---
+++ om/tests/test_meg_twin.py
@@ -0,0 +1,28 @@
+"""DOCSTRING"""
+
+from om.meg.twin import *
+
+##
+##
+##
+
+def test_get_twin_data():
+    pass
+
+def test_match_twins():
+    pass
+
+def test_check_complete_pairs():
+    pass
+
+def test_rm_twin_pairs():
+    pass
+
+def test_load_pair():
+    pass
+
+def test_compare_pair():
+    pass
+
+def test_compare_slope():
+    pass
