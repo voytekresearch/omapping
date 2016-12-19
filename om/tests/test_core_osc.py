6508e55b28687d7740e3fc0bf7d65c2020e11466
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-19 07:11:19 -0500

    Fix up tests after updates to Osc

--- om/tests/test_core_osc.py
+++ om/tests/test_core_osc.py
@@ -3,6 +3,7 @@
 from py.test import raises
 
 from om.core.osc import *
+from om.core.osc import _check_band
 
 #########################################################################################
 ########################## TESTS - OMEGAMAPPIN - CLASSES - OSC ##########################
@@ -20,31 +21,43 @@ def test_add_band():
 
     osc = Osc()
 
-    osc.add_band('test', [0, 100])
+    osc.add_band('test', (0, 100))
 
     assert 'test' in osc.bands.keys()
     assert osc.bands['test'][0] == 0
     assert osc.bands['test'][1] == 100
-
-def test_osc_add_band_error():
-    """Test that Osc.add_band() returns errors appropriately."""
-
-    osc = Osc()
-
-    with raises(InconsistentDataError):
-        osc.add_band('bad', [0, 0])
-
-    with raises(InconsistentDataError):
-        osc.add_band('worse', [10, 5])
 
 def test_rm_band():
     """Test that Osc.rm_band() removes bands properly."""
 
     osc = Osc()
 
-    osc.add_band('test', [1, 2])
+    osc.add_band('test', (1, 2))
 
     osc.rm_band('test')
 
     with raises(KeyError):
         assert osc.bands['test']
+
+##
+##
+##
+
+def test_check_band():
+    """Test that _check_band returns errors appropriately."""
+
+    # Check for error checking that key is a string
+    with raises(InconsistentDataError):
+        _check_band(111, (5, 10))
+
+    # Check for error checking that limits have length 2
+    with raises(InconsistentDataError):
+        _check_band('string', (5, 10, 15))
+
+    # Check for proper band definitions & order - 1
+    with raises(InconsistentDataError):
+        _check_band('bad', (0, 0))
+
+    # Check for proper band definitions & order - 2
+    with raises(InconsistentDataError):
+        _check_band('worse', (10, 5))
