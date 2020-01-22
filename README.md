ed1403d43e02aef458bd8fe6d09df88cda19b6d8
TomDonoghue <tdonoghue@ucsd.edu>
2020-01-22 12:27:47 -0800

    Updates to make & describe as a package

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-# omegamappin
+# omapping
 
-This project has two main objectives:
-- Use the FOOF method to quantify oscillations across the cortex in MEG data. 
-- Compare the electrophysiology maps with other data types. 
-
-Code:
-- om: Custom python code to run and analyze MEG-FOOF, and compare to other datasets.
-- scripts: Standalone scripts, for example to launch FOOF, or set visualizations.
-- preprocessing: Scripts to process MEG data.
-- notebooks: IPython notebooks demonstrating the code and results.
-- Torben: Some notebooks developed by Torben to look at MEG-FOOF data.
-
-Data:
-- Electrophysiology (MEG from OMEGA & HCP)
-- Functional (Neurosynth)
-- Genetic (Allen Brain Atlas)
-- Structural (HCP Data): collaboration with Verstynen Lab
+`om` is a package for spatial analysis of MEG and other neuroscientific data.
--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-"""NAME setup script."""
+"""Setup script for `om`."""
 
 import os
 from setuptools import setup, find_packages
 
 # Get the current version number from inside the module
-with open(os.path.join('NAME', 'version.py')) as vf:
+with open(os.path.join('om', 'version.py')) as vf:
     exec(vf.read())
 
 long_description = \
 """
+Spatial mapping of MEG and other neuroscientific data.
 """
 
 setup(
-    name = 'NAME',
+    name = 'om',
     version = __version__,
-    description = 'NAME',
+    description = long_description,
     long_description = long_description,
     author = 'Thomas Donoghue',
     author_email = 'tdonoghue.research@gmail.com',
-    url = 'https://github.com/voytekresearch/omegamappin',
+    url = 'https://github.com/voytekresearch/omapping',
     packages = find_packages(),
     license = 'Apache License, 2.0',
     classifiers = [
@@ -35,7 +36,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
-    download_url = 'https://github.com/voytekresearch/omegamappin/releases',
+    download_url = 'https://github.com/voytekresearch/omapping/releases',
     keywords = ['neuroscience', 'neural oscillations', 'power spectra', '1/f', 'electrophysiology'],
     install_requires = ['numpy', 'scipy', 'pandas', 'seaborn', 'matplotlib', 'fooof'],
     tests_require = ['pytest'],
